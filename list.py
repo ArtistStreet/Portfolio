e789dd08142be2e4a1b3c18e91b5b74807ea9496
Hải Đăng Hàn <darkstarcfvn.com>
2025-04-27 01:57:31 +0700

    Update

--- list.py
+++
@@ -1,8 +0,0 @@
-import os 
-forder = r'E:\\code\\web\\img'
-def listDir(dir):
-    fileNames = os.listdir(dir)
-    for fileName in fileNames:
-        print("./"+fileName)
-if __name__ == '__main__':
-    listDir(forder)
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function locomotive() {
 
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
-    smooth: true ,
+    smooth: true,
   });
   locoScroll.on("scroll", ScrollTrigger.update);
 
@@ -48,312 +48,231 @@ window.addEventListener("resize", function () {
 
 function files(index) {
   var data = `
-  ./0003.png
-  ./0003.png
-  ./0003.png
-  ./0003.png
-  ./0004.png
-  ./0005.png
-  ./0006.png
-  ./0007.png
-  ./0008.png
-  ./0009.png
-  ./0010.png
-  ./0011.png
-  ./0012.png
-  ./0013.png
-  ./0014.png
-  ./0015.png
-  ./0016.png
-  ./0017.png
-  ./0018.png
-  ./0019.png
-  ./0020.png
-  ./0021.png
-  ./0022.png
-  ./0023.png
-  ./0024.png
-  ./0025.png
-  ./0026.png
-  ./0027.png
-  ./0028.png
-  ./0029.png
-  ./0030.png
-  ./0031.png
-  ./0032.png
-  ./0033.png
-  ./0034.png
-  ./0035.png
-  ./0036.png
-  ./0037.png
-  ./0038.png
-  ./0039.png
-  ./0040.png
-  ./0041.png
-  ./0042.png
-  ./0043.png
-  ./0044.png
-  ./0045.png
-  ./0046.png
-  ./0047.png
-  ./0048.png
-  ./0049.png
-  ./0050.png
-  ./0051.png
-  ./0052.png
-  ./0053.png
-  ./0054.png
-  ./0055.png
-  ./0056.png
-  ./0057.png
-  ./0058.png
-  ./0059.png
-  ./0060.png
-  ./0061.png
-  ./0062.png
-  ./0063.png
-  ./0064.png
-  ./0065.png
-  ./0066.png
-  ./0067.png
-  ./0068.png
-  ./0069.png
-  ./0070.png
-  ./0071.png
-  ./0072.png
-  ./0073.png
-  ./0074.png
-  ./0075.png
-  ./0076.png
-  ./0077.png
-  ./0078.png
-  ./0079.png
-  ./0080.png
-  ./0081.png
-  ./0082.png
-  ./0083.png
-  ./0084.png
-  ./0085.png
-  ./0086.png
-  ./0087.png
-  ./0088.png
-  ./0089.png
-  ./0090.png
-  ./0091.png
-  ./0092.png
-  ./0093.png
-  ./0094.png
-  ./0095.png
-  ./0096.png
-  ./0097.png
-  ./0098.png
-  ./0099.png
-  ./0100.png
-  ./0101.png
-  ./0102.png
-  ./0103.png
-  ./0104.png
-  ./0105.png
-  ./0106.png
-  ./0107.png
-  ./0108.png
-  ./0109.png
-  ./0110.png
-  ./0111.png
-  ./0112.png
-  ./0113.png
-  ./0114.png
-  ./0115.png
-  ./0116.png
-  ./0117.png
-  ./0118.png
-  ./0119.png
-  ./0120.png
-  ./0121.png
-  ./0122.png
-  ./0123.png
-  ./0124.png
-  ./0125.png
-  ./0126.png
-  ./0127.png
-  ./0128.png
-  ./0129.png
-  ./0130.png
-  ./0131.png
-  ./0132.png
-  ./0133.png
-  ./0134.png
-  ./0135.png
-  ./0136.png
-  ./0137.png
-  ./0138.png
-  ./0139.png
-  ./0140.png
-  ./0141.png
-  ./0142.png
-  ./0143.png
-  ./0144.png
-  ./0145.png
-  ./0146.png
-  ./0147.png
-  ./0148.png
-  ./0149.png
-  ./0150.png
-  ./0151.png
-  ./0152.png
-  ./0153.png
-  ./0154.png
-  ./0155.png
-  ./0156.png
-  ./0157.png
-  ./0158.png
-  ./0159.png
-  ./0160.png
-  ./0161.png
-  ./0162.png
-  ./0163.png
-  ./0164.png
-  ./0165.png
-  ./0166.png
-  ./0167.png
-  ./0168.png
-  ./0169.png
-  ./0170.png
-  ./0171.png
-  ./0172.png
-  ./0173.png
-  ./0174.png
-  ./0175.png
-  ./0176.png
-  ./0177.png
-  ./0178.png
-  ./0179.png
-  ./0180.png
-  ./0181.png
-  ./0182.png
-  ./0183.png
-  ./0184.png
-  ./0185.png
-  ./0186.png
-  ./0187.png
-  ./0188.png
-  ./0189.png
-  ./0190.png
-  ./0191.png
-  ./0192.png
-  ./0193.png
-  ./0194.png
-  ./0195.png
-  ./0196.png
-  ./0197.png
-  ./0198.png
-  ./0199.png
-  ./0200.png
-  ./0201.png
-  ./0202.png
-  ./0203.png
-  ./0204.png
-  ./0205.png
-  ./0206.png
-  ./0207.png
-  ./0208.png
-  ./0209.png
-  ./0210.png
-  ./0211.png
-  ./0212.png
-  ./0213.png
-  ./0214.png
-  ./0215.png
-  ./0216.png
-  ./0217.png
-  ./0218.png
-  ./0219.png
-  ./0220.png
-  ./0221.png
-  ./0222.png
-  ./0223.png
-  ./0224.png
-  ./0225.png
-  ./0226.png
-  ./0227.png
-  ./0228.png
-  ./0229.png
-  ./0230.png
-  ./0231.png
-  ./0232.png
-  ./0233.png
-  ./0234.png
-  ./0235.png
-  ./0236.png
-  ./0237.png
-  ./0238.png
-  ./0239.png
-  ./0240.png
-  ./0241.png
-  ./0242.png
-  ./0243.png
-  ./0244.png
-  ./0245.png
-  ./0246.png
-  ./0247.png
-  ./0248.png
-  ./0249.png
-  ./0250.png
-  ./0251.png
-  ./0252.png
-  ./0253.png
-  ./0254.png
-  ./0255.png
-  ./0256.png
-  ./0257.png
-  ./0258.png
-  ./0259.png
-  ./0260.png
-  ./0261.png
-  ./0262.png
-  ./0263.png
-  ./0264.png
-  ./0265.png
-  ./0266.png
-  ./0267.png
-  ./0268.png
-  ./0269.png
-  ./0270.png
-  ./0271.png
-  ./0272.png
-  ./0273.png
-  ./0274.png
-  ./0275.png
-  ./0276.png
-  ./0277.png
-  ./0278.png
-  ./0279.png
-  ./0280.png
-  ./0281.png
-  ./0282.png
-  ./0283.png
-  ./0284.png
-  ./0285.png
-  ./0286.png
-  ./0287.png
-  ./0288.png
-  ./0289.png
-  ./0290.png
-  ./0291.png
-  ./0292.png
-  ./0293.png
-  ./0294.png
-  ./0295.png
-  ./0296.png
-  ./0297.png
-  ./0298.png
-  ./0299.png
-  ./0300.png
+  Selected/frame_0000.png
+Selected/frame_0001.png
+Selected/frame_0002.png
+Selected/frame_0003.png
+Selected/frame_0004.png
+Selected/frame_0005.png
+Selected/frame_0006.png
+Selected/frame_0007.png
+Selected/frame_0008.png
+Selected/frame_0009.png
+Selected/frame_0010.png
+Selected/frame_0011.png
+Selected/frame_0012.png
+Selected/frame_0013.png
+Selected/frame_0014.png
+Selected/frame_0015.png
+Selected/frame_0016.png
+Selected/frame_0017.png
+Selected/frame_0018.png
+Selected/frame_0019.png
+Selected/frame_0020.png
+Selected/frame_0021.png
+Selected/frame_0022.png
+Selected/frame_0023.png
+Selected/frame_0024.png
+Selected/frame_0025.png
+Selected/frame_0026.png
+Selected/frame_0027.png
+Selected/frame_0028.png
+Selected/frame_0029.png
+Selected/frame_0030.png
+Selected/frame_0031.png
+Selected/frame_0032.png
+Selected/frame_0033.png
+Selected/frame_0034.png
+Selected/frame_0035.png
+Selected/frame_0036.png
+Selected/frame_0037.png
+Selected/frame_0038.png
+Selected/frame_0039.png
+Selected/frame_0040.png
+Selected/frame_0041.png
+Selected/frame_0042.png
+Selected/frame_0043.png
+Selected/frame_0044.png
+Selected/frame_0045.png
+Selected/frame_0046.png
+Selected/frame_0047.png
+Selected/frame_0048.png
+Selected/frame_0049.png
+Selected/frame_0050.png
+Selected/frame_0051.png
+Selected/frame_0052.png
+Selected/frame_0053.png
+Selected/frame_0054.png
+Selected/frame_0055.png
+Selected/frame_0056.png
+Selected/frame_0057.png
+Selected/frame_0058.png
+Selected/frame_0059.png
+Selected/frame_0060.png
+Selected/frame_0061.png
+Selected/frame_0062.png
+Selected/frame_0063.png
+Selected/frame_0064.png
+Selected/frame_0065.png
+Selected/frame_0066.png
+Selected/frame_0067.png
+Selected/frame_0068.png
+Selected/frame_0069.png
+Selected/frame_0070.png
+Selected/frame_0071.png
+Selected/frame_0072.png
+Selected/frame_0073.png
+Selected/frame_0074.png
+Selected/frame_0075.png
+Selected/frame_0076.png
+Selected/frame_0077.png
+Selected/frame_0078.png
+Selected/frame_0079.png
+Selected/frame_0080.png
+Selected/frame_0081.png
+Selected/frame_0082.png
+Selected/frame_0083.png
+Selected/frame_0084.png
+Selected/frame_0085.png
+Selected/frame_0086.png
+Selected/frame_0087.png
+Selected/frame_0088.png
+Selected/frame_0089.png
+Selected/frame_0090.png
+Selected/frame_0091.png
+Selected/frame_0092.png
+Selected/frame_0093.png
+Selected/frame_0094.png
+Selected/frame_0095.png
+Selected/frame_0096.png
+Selected/frame_0097.png
+Selected/frame_0098.png
+Selected/frame_0099.png
+Selected/frame_0100.png
+Selected/frame_0101.png
+Selected/frame_0102.png
+Selected/frame_0103.png
+Selected/frame_0104.png
+Selected/frame_0105.png
+Selected/frame_0106.png
+Selected/frame_0107.png
+Selected/frame_0108.png
+Selected/frame_0109.png
+Selected/frame_0110.png
+Selected/frame_0111.png
+Selected/frame_0112.png
+Selected/frame_0113.png
+Selected/frame_0114.png
+Selected/frame_0115.png
+Selected/frame_0116.png
+Selected/frame_0117.png
+Selected/frame_0118.png
+Selected/frame_0119.png
+Selected/frame_0120.png
+Selected/frame_0121.png
+Selected/frame_0122.png
+Selected/frame_0123.png
+Selected/frame_0124.png
+Selected/frame_0125.png
+Selected/frame_0126.png
+Selected/frame_0127.png
+Selected/frame_0128.png
+Selected/frame_0129.png
+Selected/frame_0130.png
+Selected/frame_0131.png
+Selected/frame_0132.png
+Selected/frame_0133.png
+Selected/frame_0134.png
+Selected/frame_0135.png
+Selected/frame_0136.png
+Selected/frame_0137.png
+Selected/frame_0138.png
+Selected/frame_0139.png
+Selected/frame_0140.png
+Selected/frame_0141.png
+Selected/frame_0142.png
+Selected/frame_0143.png
+Selected/frame_0144.png
+Selected/frame_0145.png
+Selected/frame_0146.png
+Selected/frame_0147.png
+Selected/frame_0148.png
+Selected/frame_0149.png
+Selected/frame_0150.png
+Selected/frame_0151.png
+Selected/frame_0152.png
+Selected/frame_0153.png
+Selected/frame_0154.png
+Selected/frame_0155.png
+Selected/frame_0156.png
+Selected/frame_0157.png
+Selected/frame_0158.png
+Selected/frame_0159.png
+Selected/frame_0160.png
+Selected/frame_0161.png
+Selected/frame_0162.png
+Selected/frame_0163.png
+Selected/frame_0164.png
+Selected/frame_0165.png
+Selected/frame_0166.png
+Selected/frame_0167.png
+Selected/frame_0168.png
+Selected/frame_0169.png
+Selected/frame_0170.png
+Selected/frame_0171.png
+Selected/frame_0172.png
+Selected/frame_0173.png
+Selected/frame_0174.png
+Selected/frame_0175.png
+Selected/frame_0176.png
+Selected/frame_0177.png
+Selected/frame_0178.png
+Selected/frame_0179.png
+Selected/frame_0180.png
+Selected/frame_0181.png
+Selected/frame_0182.png
+Selected/frame_0183.png
+Selected/frame_0184.png
+Selected/frame_0185.png
+Selected/frame_0186.png
+Selected/frame_0187.png
+Selected/frame_0188.png
+Selected/frame_0189.png
+Selected/frame_0190.png
+Selected/frame_0191.png
+Selected/frame_0192.png
+Selected/frame_0193.png
+Selected/frame_0194.png
+Selected/frame_0195.png
+Selected/frame_0196.png
+Selected/frame_0197.png
+Selected/frame_0198.png
+Selected/frame_0199.png
+Selected/frame_0200.png
+Selected/frame_0201.png
+Selected/frame_0202.png
+Selected/frame_0203.png
+Selected/frame_0204.png
+Selected/frame_0205.png
+Selected/frame_0206.png
+Selected/frame_0207.png
+Selected/frame_0208.png
+Selected/frame_0209.png
+Selected/frame_0210.png
+Selected/frame_0211.png
+Selected/frame_0212.png
+Selected/frame_0213.png
+Selected/frame_0214.png
+Selected/frame_0215.png
+Selected/frame_0216.png
+Selected/frame_0217.png
+
+
  `;
   return data.split("\n")[index];
 }
 
-const frameCount = 120;
+const frameCount = 260;
 
 const images = [];
 const imageSeq = {
@@ -417,30 +336,30 @@ ScrollTrigger.create({
 
 
 
-gsap.to("#page1",{
-  scrollTrigger:{
-    trigger:`#page1`,
-    start:`top top`,
-    end:`bottom top`,
-    pin:true,
-    scroller:`#main`
+gsap.to("#page1", {
+  scrollTrigger: {
+    trigger: `#page1`,
+    start: `top top`,
+    end: `bottom top`,
+    pin: true,
+    scroller: `#main`
   }
 })
-gsap.to("#page2",{
-  scrollTrigger:{
-    trigger:`#page2`,
-    start:`top top`,
-    end:`bottom top`,
-    pin:true,
-    scroller:`#main`
+gsap.to("#page2", {
+  scrollTrigger: {
+    trigger: `#page2`,
+    start: `top top`,
+    end: `bottom top`,
+    pin: true,
+    scroller: `#main`
   }
 })
-gsap.to("#page3",{
-  scrollTrigger:{
-    trigger:`#page3`,
-    start:`top top`,
-    end:`bottom top`,
-    pin:true,
-    scroller:`#main`
-  }
-})
+// gsap.to("#page3", {
+//   scrollTrigger: {
+//     trigger: `#page3`,
+//     start: `top top`,
+//     end: `bottom top`,
+//     pin: true,
+//     scroller: `#main`
+//   }
+// })
